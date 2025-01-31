b4f66fac1fb88f42e7888f5eef77ddf617f08986
Philight <Philight@users.noreply.github.com>
2025-01-31 16:20:35 +0100

    B

--- next.config.ts
+++ next.config.ts
@@ -15,13 +15,13 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  // webpack(config) {
-  //   config.module.rules.push({
-  //     test: /\.svg$/,
-  //     use: ['@svgr/webpack'],
-  //   });
-  //   return config;
-  // },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 };
 
 const exportWithNextIntl = withNextIntl(nextConfig);
--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -9,12 +9,44 @@ import { Languages, Moon, Sun } from 'lucide-react';
 
 import { AVAILABLE_LOCALES, ICONS_SIZES } from '@/utils/constants';
 import { setUserLocale } from '@/utils/server/functions/locale';
-import { Menubar, MenubarContent, MenubarItem, MenubarMenu, MenubarTrigger } from '../shadcn/menubar';
 
-const Header = () => {
+import { Menubar, MenubarContent, MenubarItem, MenubarMenu, MenubarTrigger } from '../shadcn/menubar';
+import Icon from '@/atoms/Icon';
+import type { IGenericProps, IGenericComponent } from '@/types/generic-types';
+
+// ================================================================
+
+export default function Header() {
   const t = useTranslations('navigation');
-  const { status } = useSession();
+  // const { status } = useSession();
+  // const { setTheme, theme } = useTheme();
+  // const [mounted, setMounted] = useState(false);
+
+  return (
+    <header className="flex justify-between w-full p-4 sticky top-0 left-0 right-0 z-[97]">
+      <Icon.Q2Logo className="" />
+
+      <nav className="flex items-center">
+        <li>
+          <Link href="/">{t('blog').toUpperCase()}</Link>
+        </li>
+        <li>
+          <Link href="/">{t('create_post')}</Link>
+        </li>
+      </nav>
+
+      <FloatingMenu />
+    </header>
+  );
+}
+
+interface FloatingMenuProps extends IGenericProps {}
+
+function FloatingMenu({ className }: FloatingMenuProps) {
   const { setTheme, theme } = useTheme();
+  const t = useTranslations('navigation');
+
+  const { status } = useSession();
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
@@ -22,19 +54,20 @@ const Header = () => {
   }, []);
 
   return (
-    <header className="flex justify-center p-4 fixed top-0 left-0 right-0 z-[97]">
-      <Menubar>
-        <MenubarMenu key="theme">
-          <MenubarTrigger aria-label="theme" onClick={() => setTheme(!mounted || theme === 'light' ? 'dark' : 'light')}>
-            {!mounted || theme === 'light' ? <Moon size={ICONS_SIZES.sm} /> : <Sun size={ICONS_SIZES.sm} />}
-          </MenubarTrigger>
-        </MenubarMenu>
-        <MenubarMenu key="home">
-          <MenubarTrigger>
-            <Link href="/">{t('home').toUpperCase()}</Link>
-          </MenubarTrigger>
-        </MenubarMenu>
-        {!status || status === 'authenticated' ? (
+    <Menubar className="fixed z-[101] left-1/2 -translate-x-1/2">
+      <MenubarMenu key="theme">
+        <MenubarTrigger aria-label="theme" onClick={() => setTheme(!mounted || theme === 'light' ? 'dark' : 'light')}>
+          {!mounted || theme === 'light' ? <Moon size={ICONS_SIZES.sm} /> : <Sun size={ICONS_SIZES.sm} />}
+        </MenubarTrigger>
+      </MenubarMenu>
+      {/*
+      <MenubarMenu key="home">
+        <MenubarTrigger>
+          <Link href="/">{t('home').toUpperCase()}</Link>
+        </MenubarTrigger>
+      </MenubarMenu>*/}
+
+      {/*        {!status || status === 'authenticated' ? (
           <>
             <MenubarMenu key="profile">
               <MenubarTrigger>
@@ -60,22 +93,20 @@ const Header = () => {
               </MenubarTrigger>
             </MenubarMenu>
           </>
-        )}
-        <MenubarMenu key={'lang'}>
-          <MenubarTrigger aria-label="translate">
-            <Languages size={ICONS_SIZES.sm} />
-          </MenubarTrigger>
-          <MenubarContent>
-            {AVAILABLE_LOCALES.map((locale) => (
-              <MenubarItem key={`locale_${locale.tag}`} onClick={() => setUserLocale(locale.tag)}>
-                {locale.label}
-              </MenubarItem>
-            ))}
-          </MenubarContent>
-        </MenubarMenu>
-      </Menubar>
-    </header>
-  );
-};
+        )}*/}
 
-export default Header;
+      <MenubarMenu key={'lang'}>
+        <MenubarTrigger aria-label="translate">
+          <Languages size={ICONS_SIZES.sm} />
+        </MenubarTrigger>
+        <MenubarContent>
+          {AVAILABLE_LOCALES.map((locale) => (
+            <MenubarItem key={`locale_${locale.tag}`} onClick={() => setUserLocale(locale.tag)}>
+              {locale.label}
+            </MenubarItem>
+          ))}
+        </MenubarContent>
+      </MenubarMenu>
+    </Menubar>
+  );
+}
